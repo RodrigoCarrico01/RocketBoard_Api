d63881e9ef7a4348c54b23a848464da162ed5329
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-05-20 11:29:49 +0100

    feat: creating team

--- src/controllers/teams-controller.ts
+++ src/controllers/teams-controller.ts
@@ -1,8 +1,24 @@
 import {Request, Response} from "express"
+import {prisma} from "@/database/prisma"
+import {z} from "zod"
 
 class TeamsController {
-  create(request: Request, response: Response){
-    return response.json({message: "Ok"})
+  async create(request: Request, response: Response){
+    const bodySchema = z.object({
+      name: z.string(),
+      description: z.string()
+    })
+
+    const {name, description} = bodySchema.parse(request.body)
+
+    await prisma.team.create({
+      data: {
+        name, 
+        description
+      }
+    })
+
+    return response.status(201).json()
   }
 }
 
