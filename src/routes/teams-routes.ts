18c7b9765306a9b712a84cf1ee09d2fbcb52f35e
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-05-20 11:15:40 +0100

    feat(auth): authorization working

--- src/routes/teams-routes.ts
+++ src/routes/teams-routes.ts
@@ -3,11 +3,12 @@ import { Router } from "express"
 import { TeamsController } from "@/controllers/teams-controller"
 
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization"
 
 const teamsRoutes = Router()
 const teamsController = new TeamsController()
 
-teamsRoutes.use(ensureAuthenticated)
+teamsRoutes.use(ensureAuthenticated, verifyUserAuthorization(["admin"]))
 
 teamsRoutes.post("/", teamsController.create)
 
