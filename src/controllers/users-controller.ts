354f908ad1ac9fb73602f09601660d084859cb9c
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-05-20 10:33:26 +0100

    feat: complete user creation with validation, hashing and database insert

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,6 +1,8 @@
 import {Request, Response} from "express"
 import {z} from "zod"
 import { hash } from "bcrypt"
+import {prisma} from "@/database/prisma"
+import { AppError } from "@/utils/AppError"
 
 class UsersController {
   async create(request: Request, response: Response){
@@ -12,9 +14,25 @@ class UsersController {
 
     const { name, email, password} = bodySchema.parse(request.body)
 
+    const userWithSameEmail = await prisma.user.findFirst({where: {email}})
+
+    if(userWithSameEmail){
+      throw new AppError("User with same email already exists")
+    }
+
     const hashedPassword = await hash(password, 8)
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      }
+    })
+
+    const {password: _, ...userWithoutPassword} = user
     
-    return response.json({message: "ok", hashedPassword})
+    return response.json(userWithoutPassword)
   }
 }
 
