c4db1a84e33e8365f2ba47bc1110e6d1b6a7429b
RodrigoCarrico01 <mr.rodrigocarrico@gmail.com>
2025-05-20 10:53:46 +0100

    feat(auth): configure environment variables and JWT authentication

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+// Archive for env validation
+
+import { z } from "zod" 
+
+const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  JWT_SECRET: z.string(),
+})
+
+export const env = envSchema.parse(process.env)
